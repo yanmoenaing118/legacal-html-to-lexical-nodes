523264253edfd786d3815d6b07466153fccbbba8
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-10-26 17:57:43 +0630

    Add image plugin

--- app/components/editor/AppEditor.tsx
+++ app/components/editor/AppEditor.tsx
@@ -78,7 +78,7 @@ export default function AppEditor() {
         <ListPlugin />
 
         <CollapsiblePlugin />
-        {/* <ImagePlugin /> */}
+        <ImagePlugin />
       </LexicalComposer>
     </div>
   );
