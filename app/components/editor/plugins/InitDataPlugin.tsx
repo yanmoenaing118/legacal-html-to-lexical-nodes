52e71ed7b43cbece74e1115a81f33651b655c42a
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-10-26 14:12:55 +0630

    custom legacy html block to lexical nodes

--- app/components/editor/plugins/InitDataPlugin.tsx
+++ app/components/editor/plugins/InitDataPlugin.tsx
@@ -2,7 +2,62 @@ import { htmlStirng } from "@/data";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useEffect, useRef } from "react";
 import { $generateHtmlFromNodes, $generateNodesFromDOM } from "@lexical/html";
-import { $getRoot, $insertNodes } from "lexical";
+
+import {
+  $createParagraphNode,
+  $createTextNode,
+  EditorState,
+  LexicalEditor,
+  $getRoot,
+  $insertNodes,
+  LexicalNode,
+} from "lexical";
+
+import { $createHeadingNode, HeadingTagType } from "@lexical/rich-text";
+
+function customGenerateNodesFromDOM(
+  editor: LexicalEditor,
+  dom: HTMLElement
+): (LexicalNode | null)[] {
+  const nodes: (LexicalNode | null)[] = [];
+
+  dom.childNodes.forEach((node) => {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      const element = node as HTMLElement;
+
+      switch (element.tagName) {
+        case "H1":
+        case "H2":
+        case "H3":
+        case "H4":
+        case "H5":
+        case "H6": {
+          const headingNode = $createHeadingNode(
+            element.tagName.toLowerCase() as HeadingTagType
+          );
+          headingNode.append($createTextNode(element.textContent || ""));
+          nodes.push(headingNode);
+          break;
+        }
+        case "P": {
+          const paragraphNode = $createParagraphNode();
+          paragraphNode.append($createTextNode(element.textContent || ""));
+          nodes.push(paragraphNode);
+          break;
+        }
+        default: {
+          // Fallback to paragraph for any unhandled tags
+          const paragraphNode = $createParagraphNode();
+          paragraphNode.append($createTextNode(element.textContent || ""));
+          nodes.push(paragraphNode);
+          break;
+        }
+      }
+    }
+  });
+
+  return nodes;
+}
 
 export default function InitDataPlugin() {
   const ref = useRef(false);
@@ -19,7 +74,10 @@ export default function InitDataPlugin() {
 
       // Once you have the DOM instance it's easy to generate LexicalNodes.
       console.log(dom);
-      const nodes = $generateNodesFromDOM(editor, dom);
+      // const nodes = $generateNodesFromDOM(editor, dom);
+      const nodes = customGenerateNodesFromDOM(editor, dom.body).filter(
+        (node) => !!node
+      );
 
       console.log(nodes);
 
